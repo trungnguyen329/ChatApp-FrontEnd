74360938aaafd70362447ab5ce6035431db6f679
Trung <trungnd@ttc-solutions.com.vn>
2021-04-28 19:53:38 +0700

    oke

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      SignInComponent,
+      SignUpComponent
+   ],
   imports: [
     BrowserModule
   ],
